f2b8032d9b378a0bd3ae57facc93703a7d95d692
Kadir Tugan <ktugan@users.noreply.github.com>
2015-12-29 16:17:25 +0100

    loading data file

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .controller('View1Ctrl', ['$http', '$scope', function ($http, $scope) {
+        $scope.data = null;
 
-.controller('View1Ctrl', [function() {
+        // loading data
+        $http({
+            method: 'GET',
+            url: "./data/courses.json"
+        }).success(function (result) {
+            $scope.data = result;
+        }).error(function (data) {
+            console.log("Request failed");
+        });
 
-}]);
+    }]);
